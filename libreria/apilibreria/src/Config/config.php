71797be1ba0b65bdc60bfbf22244289df3056856
iona27 <pillancariona@gmail.com>
2021-03-22 21:32:01 +0100

    Configuracion libreria

--- libreria/apilibreria/src/Config/config.php
+++ libreria/apilibreria/src/Config/config.php
@@ -2,10 +2,10 @@
 namespace App\Config;
 
 interface config{
-  CONST DB_NAME ="libreria";
+  CONST DB_NAME ="Libreria";
   CONST DB_HOST ="localhost";
   const DB_PORT ="3306";
-  CONST DB_USER ="root";
-  CONST DB_PASSWD ="";
+  CONST DB_USER ="ionaa";
+  CONST DB_PASSWD ="P@ssw0rd";
   CONST DB_CHARSET ="utf8";
 }
