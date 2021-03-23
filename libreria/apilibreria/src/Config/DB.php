83e63b8b2535a1298132e016270b716eec0e14cf
iona27 <pillancariona@gmail.com>
2021-03-23 11:41:28 +0100

    app

--- libreria/apilibreria/src/Config/DB.php
+++ libreria/apilibreria/src/Config/DB.php
@@ -15,7 +15,6 @@ class DB {
         ";dbname=".config::DB_NAME.
         ";port=".config::DB_PORT.
         ";charset=".config::DB_CHARSET;
-        echo $dsn. " ". config::DB_USER. " ".config::DB_PASS ;
          $this->pdo = new PDO($dsn, config::DB_USER, config::DB_PASS);
         
     }
